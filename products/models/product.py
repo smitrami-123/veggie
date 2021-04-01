bbfd73328dcddf5cfb8280ad7a70c9e49a664b51
Smit Rami <smitrami6661@gmail.com>
2021-04-01 11:39:29 +0530

    Product image can be added now

--- products/models/product.py
+++ products/models/product.py
@@ -4,5 +4,5 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length=30)
     product_price = models.IntegerField(default=0)
-    #product_img = models.ImageField(upload_to='uploads/products/', default='alter.png')
+    product_img = models.ImageField(upload_to='uploads/products/', default='alter.png')
     product_des = models.CharField(max_length=300, default='')
