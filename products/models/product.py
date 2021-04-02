1615ce2862e05e3fe8f677717a0129ac8816df13
Jaypal Mudaliyar <jaypal.mudaliyar24@gmail.com>
2021-04-02 19:07:15 +0530

    Check 2

--- products/models/product.py
+++ products/models/product.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     product_price = models.IntegerField(default=0)
     product_img = models.ImageField(upload_to='products/', default='alter.png')
     product_des = models.CharField(max_length=300, default='')
-    product_ratings = models.FloatField(default=5.0,validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
+    product_ratings = models.FloatField(default=5.0, validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
