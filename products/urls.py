3acf8eb81a7f208139f7cf9f5dd3bbae377e403d
Jaypal Mudaliyar <jaypal.mudaliyar24@gmail.com>
2021-04-02 17:10:30 +0530

    Check

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import product
 
+app_name = 'products'
+
 urlpatterns = [
     # /product/
     path('', product, name='product'),
