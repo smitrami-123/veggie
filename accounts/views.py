bd2ac18d0004dd5d8f73d3343b4ee5e0b37ed9a4
Smit Rami <smitrami6661@gmail.com>
2021-04-04 20:56:19 +0530

    signup Working

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+from .forms import CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def SignUp(request):
-
-    return  render(request,'accounts/signup.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return  render(request, 'accounts/signup.html', context)
 
 def SignIn(request):
     return render(request,'accounts/login.html')
