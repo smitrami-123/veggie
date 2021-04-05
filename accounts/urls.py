be36c882afde9c3c01ba2706b0146f274395a538
Smit Rami <smitrami6661@gmail.com>
2021-04-05 14:09:22 +0530

    Login/logout Working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignIn,SignUp
+from .views import SignIn,SignUp,SignOut,userInfo
 
 urlpatterns = [
    path('SignIn/', SignIn),
    path('SignUp/', SignUp),
+   path('userinfo/',userInfo),
+   path('SignOut/', SignOut,name = 'logout'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -19,7 +20,24 @@ def SignUp(request):
 
 
 def SignIn(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request,email=email, password=password)
+        if user is not None :
+            login(request,user)
+            return redirect('/home/')
+        else:
+            messages.info(request, 'Credentials are incorrect')
     return render(request, 'accounts/login.html')
+
+def SignOut(request):
+    logout(request)
+    return redirect('/account/SignIn/')
+
+login_required(login_url='/account/SignIn/')
+def userInfo(request):
+    return render(request,'accounts/userinfo.html')
 
 # def register(request):
 #     form = CreateUserForm()
